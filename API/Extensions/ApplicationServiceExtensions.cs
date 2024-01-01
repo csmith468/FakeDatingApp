fd8fc8bbc86b83793a45a4f0d0e58273df103fe4
csmith468 <chapin2816@gmail.com>
2023-12-31 18:18:10 -0600

    Pre-Deployment

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,9 +10,6 @@ namespace API.Extensions {
 
     public static class ApplicationServiceExtensions {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) {
-            services.AddDbContext<DataContext>(opt => {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
             services.AddCors();
             
             services.AddScoped<ITokenService, TokenService>();
--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,32 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
+var connString = "";
+if (builder.Environment.IsDevelopment())
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else {
+    // Use connection string provided at runtime by FlyIO
+    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    // Parse connection URL to connection string for Npgsql
+    connUrl = connUrl.Replace("postgres://", string.Empty);
+    var pgUserPass = connUrl.Split("@")[0];
+    var pgHostPortDb = connUrl.Split("@")[1];
+    var pgHostPort = pgHostPortDb.Split("/")[0];
+    var pgDb = pgHostPortDb.Split("/")[1];
+    var pgUser = pgUserPass.Split(":")[0];
+    var pgPass = pgUserPass.Split(":")[1];
+    var pgHost = pgHostPort.Split(":")[0];
+    var pgPort = pgHostPort.Split(":")[1];
+    var updatedHost = pgHost.Replace("flycast", "internal");
+
+    connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+}
+builder.Services.AddDbContext<DataContext>(opt => {
+    opt.UseNpgsql(connString);
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,9 +53,13 @@ app.UseCors(policy => policy
 app.UseAuthentication(); // Do you have a valid token?
 app.UseAuthorization();  // What are you allowed to do?
 
+app.UseDefaultFiles();  // looks for index.html
+app.UseStaticFiles();   // looks for wwwroot folder
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -38,7 +68,7 @@ try {
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
+    await Seed.ClearConnections(context);
     await Seed.SeedUsers(userManager, roleManager);
 } catch (Exception ex) {
     var logger = services.GetService<ILogger<Program>>();
