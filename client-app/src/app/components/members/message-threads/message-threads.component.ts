53153d09fe4309c628c2a731d47485ea706d1a00
csmith468 <chapin2816@gmail.com>
2023-12-31 19:35:20 -0600

    Section 19: Final Deployment

--- client-app/src/app/components/members/message-threads/message-threads.component.ts
+++ client-app/src/app/components/members/message-threads/message-threads.component.ts
@@ -17,6 +17,7 @@ export class MessageThreadsComponent implements OnInit {
   @ViewChild('messageForm') messageForm?: NgForm
   @Input() username?: string;
   messageContent = '';
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -25,9 +26,10 @@ export class MessageThreadsComponent implements OnInit {
 
   sendMessage() {
     if (!this.username) return;
+    this.loading = true;
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       this.messageForm?.reset();
-    })
+    }).finally(() => this.loading = false);
   }
 
 }
